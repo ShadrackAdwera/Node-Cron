398443e336c65cd717f71da4d8b1ea3d2c095b8a
ShadrackAdwera <adweshshaddie@gmail.com>
2022-03-02 08:06:52 +0300

    add task status fields to task model

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,20 +1,24 @@
 import { model, Model, Document, Schema } from 'mongoose';
 import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+import { TaskStatus } from '../utils/TaskStatus';
 
 interface TaskDoc extends Document {
     title: string;
     version: number;
+    status: string;
     assignedTo: string | undefined;
 }
 
 interface TaskModel extends Model<TaskDoc> {
     title: string;
+    status: string;
     assignedTo: string | undefined;
 }
 
 const taskSchema = new Schema({
     title: { type: String, required: true },
-    assignedTo: { type: Schema.Types.ObjectId }
+    assignedTo: { type: Schema.Types.ObjectId },
+    status: { type: String, required: true, enum: Object.keys(TaskStatus), default: TaskStatus.Pending }
 }, { timestamps: true, toJSON: { getters: true } });
 
 taskSchema.set('versionKey', 'version');
---
+++ src/utils/TaskStatus.ts
@@ -0,0 +1,5 @@
+export enum TaskStatus {
+    Pending='task:pending',
+    Assigned='task:assigned',
+    Resolved='task:resolved'
+}
