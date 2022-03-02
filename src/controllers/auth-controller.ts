6bf5ac27620d2916b29e35b65ad0ac7c703824ef
ShadrackAdwera <adweshshaddie@gmail.com>
2022-03-02 08:11:30 +0300

    create sign up controller

---
+++ src/controllers/auth-controller.ts
@@ -0,0 +1,62 @@
+import brypto from 'crypto';
+import { HttpError } from '@adwesh/common';
+import { Request, Response, NextFunction } from 'express';
+import { validationResult } from 'express-validator';
+import { natsWraper } from '@adwesh/common';
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken';
+
+import { User } from '../models/User';
+
+const signUp = async(req: Request, res: Response, next: NextFunction) => {
+    const error = validationResult(req);
+    if(!error.isEmpty()) {
+        return next(new HttpError('Invalid inputs', 422));
+    }
+    let foundUser;
+    //let foundSection;
+    let hashedPassword: string;
+    let token: string;
+    const { email, password } = req.body;
+
+    //check if email exists in the DB
+    try {
+        foundUser = await User.findOne({email}).exec();
+    } catch (error) {
+        return next(new HttpError('An error occured, try again', 500));
+    }
+    if(foundUser) {
+        return next(new HttpError('Email exists, login instead', 400));
+    }
+    
+    //hash password
+    try {
+        hashedPassword = await bcrypt.hash(password, 12);
+    } catch (error) {
+        return next(new HttpError('An error occured, try again', 500));
+    }
+
+
+    // create new user
+    const newUser = new User({
+        email, 
+        password: hashedPassword, 
+        tasks: []
+    });
+
+    try {
+        await newUser.save();
+    } catch (error) {
+        return next(new HttpError('An error occured, try again', 500));
+    }
+
+    try {
+        token = await jwt.sign( { id: newUser.id, email }, process.env.JWT_KEY!, { expiresIn: '1h' });    
+    } catch (error) {
+        return next(new HttpError('An error occured, try again', 500));
+    }
+
+    res.status(201).json({message: 'Sign Up successful', user: { id: newUser.id, email, token }});
+
+}
+
