ce0b1cd7a01fe617c2204dca05e2f2c52b68d1d8
ShadrackAdwera <adweshshaddie@gmail.com>
2022-03-03 08:38:16 +0300

    create user roles enum

--- cron/src/utils/TaskStatus.ts
+++ cron/src/utils/TaskStatus.ts
@@ -3,3 +3,9 @@ export enum TaskStatus {
     Assigned='task:assigned',
     Resolved='task:resolved'
 }
+
+export enum UserRoles {
+    Agent='role:agent',
+    Admin='role:admin',
+    User='role:user'
+}
